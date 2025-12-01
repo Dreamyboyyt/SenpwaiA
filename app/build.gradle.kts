657fbd979d9d7422fc10835728904655d2174297
Dreamyboyyt <pkpakisanp804.gmail.com>
2025-12-01 19:48:16 +0500

    Hellow

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "com.sleepy.senpwaia"
-    compileSdk = 35
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.sleepy.senpwaia"
         minSdk = 21
-        targetSdk = 35
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
